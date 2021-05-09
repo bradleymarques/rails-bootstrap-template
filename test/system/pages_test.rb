3b02af169c31d27e2c1892682e66f287b0fb6011
Bradley Marques <brad@next45.co>
2021-05-09 06:36:21 +0200

    System tests

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -6,4 +6,29 @@ class PagesTest < ApplicationSystemTestCase
     visit(root_url)
     assert_selector("h1", text: I18n.t("app_name"))
   end
+
+  test "a user can visit the root URL and see the app's description" do
+    visit(root_url)
+    assert_selector("p", text: I18n.t("app_description"))
+  end
+
+  test "a user can visit the root URL and see a link to GitHub" do
+    visit(root_url)
+    assert_selector("a", text: I18n.t("home.github"))
+  end
+
+  test "a user can visit the root URL and see a link to the software license" do
+    visit(root_url)
+    assert_selector("a", text: I18n.t("home.license"))
+  end
+
+  test "a user can visit the root URL and see a link to login" do
+    visit(root_url)
+    assert_selector("a", text: I18n.t("login"))
+  end
+
+  test "a user can visit the root URL and see a link to register" do
+    visit(root_url)
+    assert_selector("a", text: I18n.t("register"))
+  end
 end
